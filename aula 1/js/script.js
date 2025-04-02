a0af71e2de35bccdd78bcc402877cb9624ea4ee6
sydneysantos12 <samuelsantos.sud07@gmail.com>
2025-04-02 19:18:15 -0300

    version1
    adição a javascript

---
+++ aula 1/js/script.js
@@ -0,0 +1,15 @@
+document.getElementById("demo").innerHTML= "pedrão";
+
+function aumentar (){
+    document.getElementById("demo").style.fontSize = "35px";
+}
+function diminuir (){
+    document.getElementById("demo").style.fontSize = "15px";
+}
+function desligar(){
+    document.getElementById("lampada").src = "img/lampada desligada].jpg";
+}
+
+function ligar(){
+    document.getElementById("lampada").src = "img/lampada ligada.jpg";
+}
