f5b21344bde913c944b26cba5f95ff9858aa6641
sydneysantos12 <samuelsantos.sud07@gmail.com>
2025-04-03 17:40:17 -0300

    v2

---
+++ aula 1/js/script copy.js
@@ -0,0 +1,15 @@
+document.getElementById("demo").innerHTML= "pedrão";
+
+function aumentar (){
+    document.getElementById("demo").style.fontSize = "35px";
+}
+function diminuir (){
+    document.getElementById("demo").style.fontSize = "15px";
+}
+function dia(){
+    document.getElementById("ceu").src="img/imgdia.jpg" ;style="width: 250px";
+}
+
+function chuva(){
+    document.getElementById("ceu").src = "img/imagem de chuva.jpg";
+}
