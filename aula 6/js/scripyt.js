0e01a05a787002968fa619dfac4d018b89b2b22a
sydneysantos12 <samuelsantos.sud07@gmail.com>
2025-04-14 16:14:56 -0300

    aula 6

---
+++ aula 6/js/scripyt.js
@@ -0,0 +1,10 @@
+document.getElementById("btnmulher").addEventListener("click", function(){
+    document.getElementById("header").style.backgroundColor = "blueviolet";
+    document.getElementById("footer").style.backgroundColor = "blueviolet";
+
+});
+document.getElementById("btnhomem").addEventListener("click", function(){
+    document.getElementById("header").style.backgroundColor = "blue";
+    document.getElementById("footer").style.backgroundColor = "blue";
+
+});
